89bf5269fc58ae64ef6c8c7c2d6baeb147a41c1c
Matthew Keller <matthew.ed.keller@gmail.com>
2022-03-30 08:32:25 -0400

    introduce graph vc service

---
+++ example/web/src/GraphVcService.ts
@@ -0,0 +1,9 @@
+export function initialize() {
+  let isChannelReady = false;
+  let isInitiator = false;
+  let isStarted = false;
+  let localStream;
+  let pc;
+  let remoteStream;
+  let turnReady;
+}
